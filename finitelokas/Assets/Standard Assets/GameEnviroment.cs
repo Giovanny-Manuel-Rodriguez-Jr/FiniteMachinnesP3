956d427f4b6315b3364e6757f5fe16d4ca4d12c2
Giovanny Rodriguez Jr <145072297+Giovanny-Manuel-Rodriguez-Jr@users.noreply.github.com>
2025-01-27 12:00:19 -0800

    on 1 patrolling the perimeter
    on -5:45

---
+++ finitelokas/Assets/Standard Assets/GameEnviroment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnviroment : MonoBehaviour
+{
+    private static GameEnviroment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+    // Start is called before the first frame update
+   
+    public static GameEnviroment Singleton
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameEnviroment();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+            return instance;
+        }
+    }
+}
